f6591c2d21d139b5e3d705f7e0accccccd429f5a
marxistpokemon <enric.llagostera@gmail.com>
2013-09-17 02:50:42 -0300

    Initial integration with flippant.js
    I'm enjoying this more. It gives some natural separation between editing
    and viewing. Now I need to find out how to "save" for real.

--- js/carteando.js
+++ js/carteando.js
@@ -7,7 +7,7 @@ var defaultCard = {id:0, title:"Default Title"};
 
 $.fn.editable.defaults.mode = 'inline';
 
-
+var front, back_content, back;
 
 
 
@@ -15,6 +15,7 @@ function Card(pId, pTitle){
 	this.id = pId;
 	this.title = pTitle;
 }
+
 
 d3.select(".new_card-btn").on("click", function(d, i){
 	console.log("new_card clicked");
@@ -28,17 +29,38 @@ d3.select(".new_card-btn").on("click", function(d, i){
 	newCard = d3.select(".card-container").selectAll(".card").data(allCards)
 		.enter().append("div").attr("class", "card col-md-3");
 
-	newCard.append("textarea").attr("class", "card-title").text(function (d, i) {
+	
+
+	newCard.append("a").attr("class", "edit_card-btn btn btn-block btn-primary").text("Edit card")
+	.on("click", function(d, i){
+		console.log("clicked edit");
+
+		back = flippant.flip(this.parentNode, 
+			"<h3 class='card-title' contentEditable='true'>Add a title</h3>"+
+			"<p class='card-description' contentEditable='true'>Add a card description</p>"+
+			"<a class='save_card-btn btn btn-block btn-primary'>Save card</a>", "card");
+	
+		$(".save_card-btn").click(function(){
+			console.log("clicked save");
+			back.close();
+		});
+	});
+
+	/*
+	newCard.append("input").attr("class", "card-title").text(function (d, i) {
 		  	return allCards[i].title;
 		}).attr("maxlength", "30").attr("contentEditable", "true");
 
-	newCard.append("textarea").attr("class", "card-description").text("Click to add a card description...")
-		.attr("maxlength", "120").attr("contentEditable", "true");
+	newCard.append("textarea").attr("class", "card-description")
+		.attr("placeholder", "Click to add a card description...")
+		.attr("maxlength", "120");
+	*/
 
 	newCard.append("p").attr("class", "id-line").text(function (d, i) {
 		  	return "Card no. " + i;
 		});
 
+	/*
 	$(".card-description").maxlength({
 	    alwaysShow: false,
 	    threshold: 10
@@ -48,6 +70,7 @@ d3.select(".new_card-btn").on("click", function(d, i){
 	    alwaysShow: false,
 	    threshold: 10
 	});
+	*/
 	
 });
 
